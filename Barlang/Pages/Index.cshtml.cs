ee022b783fe7cbdf69fff5ac45a1556215c89e23
LoneHusko <90392521+LoneHusko@users.noreply.github.com>
2025-09-30 08:02:44 +0200

    initial commit - project created

---
+++ Barlang/Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Barlang.Pages;
+
+public class IndexModel : PageModel {
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger) {
+        _logger = logger;
+    }
+
+    public void OnGet() { }
+}
